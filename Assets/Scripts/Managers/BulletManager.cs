8696256594ef3b2eac87daba3c65b3996647a56a
Brett Cutler <brettacutler@gmail.com>
2017-08-12 23:43:56 -0700

    Set up  shooting, reorganize input mappings

---
+++ Assets/Scripts/Managers/BulletManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d980da3b41971f97311eb174ca7ca6337b0adbfdb0d61ee0ac37ec0b22e14d7c
+size 248
---
+++ Assets/Scripts/Player/AimCursor.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8792ae60271e742d2c40ea9112cfe44a5c9ecad9b6d10bd0d3a2e490336c9318
+size 408
